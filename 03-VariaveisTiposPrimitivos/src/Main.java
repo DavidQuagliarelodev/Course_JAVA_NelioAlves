9df9b1494785335277d000be1803adf5352a0c3a
DavidQuagliarelodev <quagliarelodev@gmail.com>
2025-08-24 22:14:33 -0300

    FIX: add VariaveisTiposPrimitivos

---
+++ 03-VariaveisTiposPrimitivos/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        String nome = "Pedro"; //Tipo - Identificador - Valor
+        int idade = 20;
+        double peso = 70.5;
+        float altura = 1.75f; //sempre um f ao final
+        char sexo = 'M';
+        boolean estudante = true;
+        //int, douable, float, char, boolean - tipos primitivos
+    }
+}
