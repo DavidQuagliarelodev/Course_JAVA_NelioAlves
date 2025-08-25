83d4be44d678ae29929fa6d6ea5d987ec89c66cf
DavidQuagliarelodev <quagliarelodev@gmail.com>
2025-08-25 06:57:48 -0300

    FEAT: Add FuncoesMatematicas

---
+++ 06-FuncoesMatematicas/src/FuncoesMatematicas.java
@@ -0,0 +1,14 @@
+public class FuncoesMatematicas {
+
+    public double raiz(double numero){
+        return Math.sqrt(numero);
+    }
+
+    public double elevar(double numero, double numero2){
+        return Math.pow(numero, numero2);
+    }
+
+    public double absoluto(double numero){
+        return Math.abs(numero);
+    }
+}
---
+++ 06-FuncoesMatematicas/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        FuncoesMatematicas funcoesMatematicas = new FuncoesMatematicas();
+        System.out.println(funcoesMatematicas.raiz(4));
+        System.out.println(funcoesMatematicas.absoluto(10.50));
+        System.out.println(funcoesMatematicas.elevar(20, 5));
+    }
+}
