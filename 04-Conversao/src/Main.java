3e0e3a0eac1c07a120ff03387098558315b9b6c0
DavidQuagliarelodev <quagliarelodev@gmail.com>
2025-08-24 22:25:04 -0300

    FEAT: add Conversao

---
+++ 04-Conversao/src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        int a = 10;
+        double b = 20.5;
+
+        double c = (double) a;
+        int d = (int) b;
+
+        System.out.println(c);
+        System.out.println(d);
+    }
+}
