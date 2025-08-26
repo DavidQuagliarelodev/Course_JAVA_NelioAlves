d4497b568f249f38c7f327202786674e5c345c28
DavidQuagliarelodev <quagliarelodev@gmail.com>
2025-08-26 07:47:51 -0300

    FEAT: add ExpressoesLogicas

---
+++ 03-EstruturaCondicional/02-ExpressoesLogicas/src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+        public static void main(String args[ ]){
+            int numberone = 14, number2 = 23;
+            System.out.println(numberone > number2 && numberone != 13);
+            System.out.println(numberone < number2 && numberone != 13);
+            System.out.println(numberone > number2 || numberone != 13);
+            System.out.println(numberone < number2 || numberone != 13);
+            System.out.println(numberone != number2);
+            System.out.println(numberone == number2);
+
+        }
+}
+
+
