66f5167136c2556f64029ff67c9828a2e1fd8371
DavidQuagliarelodev <quagliarelodev@gmail.com>
2025-08-26 08:04:17 -0300

    FEAT: Add SwithCase

---
+++ 03-EstruturaCondicional/04-SwitchCase/src/Main.java
@@ -0,0 +1,26 @@
+public class Main {
+        public static void main(String args[ ]){
+            int menu = 43;
+
+            switch (menu){
+                case 1:
+                    System.out.println("Opcao 1");
+                    break;
+                case 2:
+                    System.out.println("Opcao 2");
+                    break;
+                case 3:
+                    System.out.println("Opcao 3");
+                    break;
+                case 4:
+                    System.out.println("Opcao 4");
+                    break;
+
+                default:
+                    System.out.println("Opcao Invalida");
+                    break;
+            }
+        }
+    }
+
+
