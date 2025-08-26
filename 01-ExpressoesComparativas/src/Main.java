bd6d64fe0722697c480785ec7bbaf057fa9e292c
DavidQuagliarelodev <quagliarelodev@gmail.com>
2025-08-26 07:40:17 -0300

    FEAT: ExpressoesComparativas

---
+++ 01-ExpressoesComparativas/src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String args[ ]){
+        int numberone = 14, number2 = 23;
+        System.out.println(numberone > number2);
+        System.out.println(numberone < number2);
+        System.out.println(numberone >= number2);
+        System.out.println(numberone <= number2);
+        System.out.println(numberone != number2);
+        System.out.println(numberone == number2);
+
+    }
+}
