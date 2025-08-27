8b50e338853bc25ff8b75a945a1e803c75c03893
DavidQuagliarelodev <quagliarelodev@gmail.com>
2025-08-27 07:50:13 -0300

    FEAT: add EstruturaRepetitiva-While

---
+++ 04-EstruturaRepetitiva/01-While/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args){
+        int i = 1;
+        //Metodo, Condicao, Estrutura e Acumulativo
+        while (i <= 10){
+            System.out.println(i);
+            i++;
+        }
+    }
+}
