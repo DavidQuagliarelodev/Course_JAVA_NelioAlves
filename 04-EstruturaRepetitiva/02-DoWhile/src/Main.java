a3a9da3bdb6bc0a731032cea0b06a884e1e56399
DavidQuagliarelodev <quagliarelodev@gmail.com>
2025-08-27 07:53:30 -0300

    FEAT: add doWhile

---
+++ 04-EstruturaRepetitiva/02-DoWhile/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args){
+        int i = 0;
+        //Mesmo a condicao sendo falsa, o DoWhile sempre garente pelo menus
+        //uma execucao
+        do {
+            System.out.println(i);
+        }while (i > 0);
+    }
+}
