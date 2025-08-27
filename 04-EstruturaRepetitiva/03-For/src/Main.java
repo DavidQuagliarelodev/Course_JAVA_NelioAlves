bcb1b14b1416c0a0ffaa280026e2cc0f5ed9cf97
DavidQuagliarelodev <quagliarelodev@gmail.com>
2025-08-27 07:59:24 -0300

    FEAT: add ForMetod

---
+++ 04-EstruturaRepetitiva/03-For/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args){
+        //For e de longe o metodo mais utilizado no mercado real
+        for(int i = 0; i <= 10; i++){
+            System.out.println("Contador: " + i);
+        }
+    }
+}
